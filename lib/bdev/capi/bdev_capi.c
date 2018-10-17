2a00f9e24602630e45c1ccea2631855fd8b942cf
Takeshi Yoshimura <tyos@jp.ibm.com>
2018-10-17 13:36:01 +0900

    fix

--- lib/bdev/capi/bdev_capi.c
+++ lib/bdev/capi/bdev_capi.c
@@ -309,7 +309,9 @@ static int capi_io_poll(void *arg)
 	uint64_t status;
 	struct spdk_bdev_io *bdev_io;
 	int pflag = 0;
+	TAILQ_HEAD(, spdk_bdev_io)	io;
 
+	TAILQ_INIT(&io);
 	TAILQ_FOREACH(bdev_io, &ch->io, module_link) {
 		struct capi_bdev * bdev = (struct capi_bdev *)bdev_io->bdev->ctxt;
 		struct capi_bdev_io *bio = (struct capi_bdev_io *)bdev_io->driver_ctx;
@@ -319,17 +321,20 @@ static int capi_io_poll(void *arg)
 			c++;
 			SPDK_DEBUGLOG(SPDK_LOG_BDEV_CAPI, "cblk_aresult(%d, %d, status, %d)=SUCCESS\n", bdev->chunk_id, bio->tag, pflag);
 			spdk_bdev_io_complete(bdev_io, SPDK_BDEV_IO_STATUS_SUCCESS);
-			TAILQ_REMOVE(&ch->io, bdev_io, module_link);
+			TAILQ_INSERT_TAIL(&io, bdev_io, module_link);
 		} else if (rc < 0) {
 			SPDK_DEBUGLOG(SPDK_LOG_BDEV_CAPI, "cblk_aresult(%d, %d, status, %d)=FAIL\n", bdev->chunk_id, bio->tag, pflag);
 			spdk_bdev_io_complete(bdev_io, SPDK_BDEV_IO_STATUS_FAILED);
-			TAILQ_REMOVE(&ch->io, bdev_io, module_link);
+			TAILQ_INSERT_TAIL(&io, bdev_io, module_link);
 		}
 		if (pflag == 0) {
 		    pflag = CBLK_ARESULT_NO_HARVEST;
 		}
 	}
 
+	TAILQ_FOREACH(bdev_io, &io, module_link) {
+		TAILQ_REMOVE(&ch->io, bdev_io, module_link);
+	}
 	return c;
 }
 
